531ea1cc9620bf9ca6ec2e2ef08e29870a9ed235
Vansh-77 <Vanshbhardwaj733@gmail.com>
2025-06-18 14:00:56 +0530

    cron job

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import "dotenv/config";
+import cors from 'cors';
+import job from './lib/cron.js';
 import AuthRoutes from "./routes/AuthRoutes.js"
 import BookRoutes from "./routes/BookRoutes.js"
 import { connectDB } from './lib/db.js';
@@ -9,6 +11,13 @@ const app = express()
 
 const Port  = process.env.PORT;
 app.use(express.json());
+app.use(cors());
+
+job.start();
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to Bookworm API")
+});
 
 app.use("/api/auth",AuthRoutes);
 app.use("/api/books",BookRoutes);
---
+++ src/lib/cron.js
@@ -0,0 +1,16 @@
+import cron from "cron";
+import https from "https";
+
+const job = new cron.CronJob("*/14 * * * *" , () => {
+  https.get(process.env.API_URL, (res) => {
+    if (res.statusCode !== 200) {
+      console.error(`Request failed with status code: ${res.statusCode}`);
+    } else {
+      console.log("Cron job executed successfully.");
+    }
+  }).on("error", (err) => {
+    console.error("Error during cron job execution:", err);
+  });
+});
+
+export default job;
